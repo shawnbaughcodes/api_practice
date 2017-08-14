e4e043393d9d28ee474dff092eb5a42141f297bc
Shawn Baugh II <shawnbaughcreative@gmail.com>
2017-08-13 19:03:01 -0700

    Finished impleminiting autocomplete google search api.

--- static/js/script.js
+++ static/js/script.js
@@ -1,28 +1,29 @@
 $(document).ready(function() {
     let moves = []
     let abilities = []
-    // $.get("http://pokeapi.co/api/v1/pokemon/1/", function(res) {
-    //     console.log(res);
-    //     // MOVES
-    //     for (let i in res.moves) {
-    //         moves.push(res.moves[i].name);
-    //         $(".moves").append(i+ ". " + moves[i] + ", ")
-    //     };
-    //     // ABILITIES
-    //     for (let i in res.abilities) {
-    //         // console.log(res.abilities[i].name);
-    //         abilities.push(res.abilities[i].name)
-    //         $(".abilities").append(i+ ". " + res.abilities[i].name + ", ")
-    //     }
-    //     $(".speed").append(res.speed)
-    //     $(".attack").append(res.attack)
-    // }, "json")
-    // $.get("", function(res) {
-    //     "user-key: ",
-    //
+    $.get("http://pokeapi.co/api/v1/pokemon/1/", function(res) {
+        console.log(res);
+        // MOVES
+        for (let i in res.moves) {
+            moves.push(res.moves[i].name);
+            $(".moves").append(i+ ". " + moves[i] + ", ")
+        };
+        // ABILITIES
+        for (let i in res.abilities) {
+            // console.log(res.abilities[i].name);
+            abilities.push(res.abilities[i].name)
+            $(".abilities").append(i+ ". " + res.abilities[i].name + ", ")
+        }
+        $(".speed").append(res.speed)
+        $(".attack").append(res.attack)
+    }, "json")
+    // function activatePlacesSearch() {
+    //     let input = document.getElementById('search_term');
+    //     let autocomplete = new google.maps.places.Autocomplete(input);
+    // }
+    // var ac = new google.maps.places.Autocomplete(document.getElementById('search'));
+    // google.maps.event.addListener(ac, 'place_changed', function(){
+    //     var place = ac.getPlace();
+    //     console.log(place.formatted_address);
     // })
-    $.ajax({
-        query: "Restaurants",
-        key: 'AIzaSyBCilcMeJ7O77FTSwvomDgl3V7UtO8uA0U',
-    })
 });
